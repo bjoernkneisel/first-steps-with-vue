726a43841d5b3a6e9cc59a7b8cd6536076daa421
bjoernkneisel <36930665+bjoernkneisel@users.noreply.github.com>
2019-04-27 22:17:14 +0200

    Snapshot | Section 3: Lection 42

--- vuejs-2-the-complete-guide/section 3/app.js
+++ vuejs-2-the-complete-guide/section 3/app.js
@@ -1,11 +1,15 @@
 new Vue({
 	el: "#app",
 	data: {
-		firstString: "Hello!"
-	},
-	computed: {
-		iReturnAString() {
-			return "hello";
-		}
+		show: true,
+		ingredients: ["meat", "fruit", "cookies"],
+		persons: [
+			{ name: "Jimmy", age: 22, haircolor: "blonde" },
+			{ name: "Sebastian", age: 21, haircolor: "darkbrown" },
+			{ name: "Micheal", age: 22, haircolor: "brown" }
+		],
+		code1: '<li v-for="(ingredient, i) in ingredients">',
+		code2: "{{ i + 1 }}: {{ ingredient }}",
+		code3: '<li v-for="(ingredient, i) in ingredients">'
 	}
 });
