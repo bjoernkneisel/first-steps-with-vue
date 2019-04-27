6a96cc2966d5ce1433a0a972fd9a74aad2ab5993
bjoernkneisel <36930665+bjoernkneisel@users.noreply.github.com>
2019-04-27 23:12:50 +0200

    V2TCG - Section 3 completed

--- vuejs-2-the-complete-guide/section 3/app.js
+++ vuejs-2-the-complete-guide/section 3/app.js
@@ -8,6 +8,13 @@ new Vue({
 			{ name: "Sebastian", age: 21, haircolor: "darkbrown" },
 			{ name: "Micheal", age: 22, haircolor: "brown" }
 		],
+		carmakes: ["BMW", "VW", "Opel", "Fiat", "Porsche", "Mercedes"],
+		bookseries: [{ title: "Lord of the Rings", author: "J.R.R. Tolkiens", books: "3" }],
+		dataArray: {
+			name: "TESTOBJECT",
+			id: 10,
+			data: [1.67, 1.33, 0.98, 2.21]
+		},
 		code1: '<li v-for="(ingredient, i) in ingredients">',
 		code2: "{{ i + 1 }}: {{ ingredient }}",
 		code3: '<li v-for="(ingredient, i) in ingredients">'
